772f3626674fd6de6b54c206269736eee971402f
Sourcery AI <>
2023-05-12 06:18:21 +0000

    'Refactored by Sourcery'

--- scripts/convert_image.py
+++ scripts/convert_image.py
@@ -18,8 +18,7 @@ def parse_args():
 	parser = argparse.ArgumentParser(description="Convert image into a different format. By default, converts to our binary fp16 '.bin' format, which helps quickly load large images.")
 	parser.add_argument("--input", default="", help="Path to the image to convert.")
 	parser.add_argument("--output", default="", help="Path to the output. Defaults to <input>.bin")
-	args = parser.parse_args()
-	return args
+	return parser.parse_args()
 
 if __name__ == "__main__":
 	args = parse_args()
@@ -29,7 +28,7 @@ if __name__ == "__main__":
 	print(f"{img.shape[1]}x{img.shape[0]} pixels, {img.shape[2]} channels")
 
 	if not args.output:
-		output = os.path.splitext(args.input)[0] + ".bin"
+		output = f"{os.path.splitext(args.input)[0]}.bin"
 	else:
 		output = args.output
 
